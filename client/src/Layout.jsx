ec428053ca10558e17df7d29fc8e6fead542a203
tomyrodeghiero <tomyrodeghiero@gmail.com>
2023-03-08 15:13:59 -0300

    Add Post

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,12 +1,40 @@
 import Header from "./Header";
-import {Outlet} from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 export default function Layout() {
+  const [posts, setPosts] = useState < any > [];
+
+  useEffect(() => {
+    fetch("https://test-clone-iota.vercel.app/api/test").then((response) => {
+      response.json().then((posts) => {
+        setPosts(posts);
+        console.log(posts);
+      });
+    });
+  }, []);
+
   return (
-    
     <div className="py-4 px-8 flex flex-col min-h-screen max-w-4xl mx-auto">
       <Header />
       <Outlet />
+      <h5 className="posts__pathname">
+        Inicio - Lista de Lecturas -
+        <span className="posts__pathname--bold"> Posts</span>
+      </h5>
+      {posts.length > 0 &&
+        posts.map((post, index) => (
+          <div className="posts__container">
+            <Card
+              _id={post._id}
+              cover={post.cover}
+              title={post.title}
+              number={index}
+              author={post.author}
+              createdAt={post.createdAt}
+              summary={post.summary}
+            />
+          </div>
+        ))}
     </div>
   );
 }
